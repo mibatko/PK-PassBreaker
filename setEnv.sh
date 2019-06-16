d07648cc9614ca5b6b7d4dbc00845eb04f8f1849
mibatko <mibatko@gmail.com>
2019-06-16 03:44:21 +0200

    Env for other machines

--- setEnv.sh
+++ setEnv.sh
@@ -1,3 +1,5 @@
+systemctl disable systemd-networkd-wait-online.service
+systemctl mask systemd-networkd-wait-online.service
 sudo apt install build-essential make wget git gcc g++
 sudo apt install pvm pvm-dev libgtk-3-dev
 cp environment /etc/environment
