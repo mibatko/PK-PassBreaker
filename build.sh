9a81fd166326178fe8ef5a5c1997d425194b8b6a
mibatko <mibatko@gmail.com>
2019-06-16 03:11:04 +0200

    Env for other machines

--- build.sh
+++ build.sh
@@ -1,4 +1,5 @@
 g++ -o master master.cpp -I$PVM_ROOT/include -L$PVM_ROOT/lib/LINUX -lpvm3 -lnsl -lcrypt
 g++ -o worker worker.cpp -I$PVM_ROOT/include -L$PVM_ROOT/lib/LINUX -lpvm3 -lnsl -lcrypt
+mkdir /usr/lib/pvm3/bin/LINUX64
 cp worker /usr/lib/pvm3/bin/LINUX64
 cp master /usr/lib/pvm3/bin/LINUX64
---
+++ setEnv.sh
@@ -0,0 +1,1 @@
+cp environment /etc/environment
